840383989a6388370c26d925b8c12c0604bd4f6f
MThe001 <mdredwanulhaque01614591672@gmail.com>
2025-02-18 23:35:38 +0600

    fix(plans-section):card-optimized

--- src/components/PlansSection.jsx
+++ src/components/PlansSection.jsx
@@ -7,10 +7,10 @@ const PlansSection = () => {
     return (
         <section className={`plans-section ${pathname === "/" ? "pt-24" : "pt-0"} pb-10 my-6 bg-background rounded-lg border-2 mx-10`}>
             <div className="md:w-11/12 w-full flex flex-col items-center justify-center p-8 rounded-xl mx-auto text-center">
-                <h2 className="lg:text-5xl md:text-4xl text-3xl text-start lg:text-center font-semibold text-gray-900 dark:text-white mb-8">
+                <h2 className="lg:text-5xl md:text-4xl  text-xl text-start lg:text-center font-semibold text-gray-900 dark:text-white mb-8">
                     Unlock Premium Benefits: Your Pathway to Exclusive Content Awaits
                 </h2>
-                <p className="text-lg text-gray-600 dark:text-gray-400 mb-12">
+                <p className="text-md text-start text-gray-600 dark:text-gray-400 mb-12">
                     Find the plan that fits you best! With premium features, you’ll unlock
                     a whole new world of content and perks.
                 </p>
@@ -71,15 +71,15 @@ const PlansSection = () => {
                                     background: "radial-gradient(circle, rgba(255,255,255,0.3) 0%, rgba(0,0,0,0.1) 70%)",
                                 }}
                             ></div>
-                            <div className="flex items-center justify-between w-full mb-4 relative z-10">
-                                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">{plan.title}</h3>
+                            <div className="flex items-center justify-between  w-full mb-4 relative z-10">
+                                <h3 className="lg:text-xl text-sm font-semibold text-gray-900 dark:text-white">{plan.title}</h3>
                                 <div className="text-right">
-                                    <span className="text-3xl font-bold text-gray-900 dark:text-white">${plan.price}</span>
+                                    <span className="lg:text-3xl md:text-3xl text-xl font-bold text-gray-900 dark:text-white">${plan.price}</span>
                                     <span className="text-sm text-gray-600 dark:text-gray-300">/mo</span>
                                 </div>
                             </div>
-                            <p className="text-gray-700 dark:text-gray-200 mb-6 relative z-10">{plan.description}</p>
-                            <ul className="text-gray-600 dark:text-gray-300 space-y-3 mb-6 relative z-10">
+                            <p className="text-gray-700 lg:text-xl lg:text-center text-sm text-start dark:text-gray-200 mb-6 relative z-10">{plan.description}</p>
+                            <ul className="text-gray-600 dark:text-gray-300 space-y-3 lg:text-xl text-sm   mb-6 relative z-10">
                                 {plan.benefits.map((benefit, i) => (
                                     <li key={i} className="flex items-center">
                                         <AiOutlineCheck className="text-green-500 dark:text-green-400 mr-2" />
@@ -96,7 +96,7 @@ const PlansSection = () => {
 
                             <Link
                                 to="/subscription"
-                                className={`mt-auto w-full text-center px-6 py-3 bg-gradient-to-r from-${plan.buttonColor}-500 to-${plan.buttonColor}-700 dark:from-${plan.buttonColor}-600 dark:to-${plan.buttonColor}-800 text-white rounded-full transform transition-all duration-300 hover:scale-105 hover:from-${plan.buttonColor}-700 hover:to-${plan.buttonHover}-500`}
+                                className={`mt-auto w-full lg:h-full  text-center text-sm px-6 py-3 bg-gradient-to-r from-${plan.buttonColor}-500 to-${plan.buttonColor}-700 dark:from-${plan.buttonColor}-600 dark:to-${plan.buttonColor}-800 text-white rounded-full transform transition-all duration-300 hover:scale-105 hover:from-${plan.buttonColor}-700 hover:to-${plan.buttonHover}-500`}
                             >
                                 Select Plan
                             </Link>
